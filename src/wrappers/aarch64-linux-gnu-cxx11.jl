50739d4c15bed7b5d9be7af00fde2f92fb9f0457
jlbuild <juliabuildbot@gmail.com>
2026-06-30 08:03:15 +0000

    pocl_next_jll build 7.2.0+7

--- src/wrappers/aarch64-linux-gnu-cxx11.jl
+++ src/wrappers/aarch64-linux-gnu-cxx11.jl
@@ -5,11 +5,12 @@ using OpenCL_jll
 using OpenCL_Headers_jll
 using Hwloc_jll
 using Zstd_jll
+using SLEEF_jll
 JLLWrappers.@generate_wrapper_header("pocl_next")
 JLLWrappers.@declare_library_product(libpocl, "libpocl.so.2")
 JLLWrappers.@declare_executable_product(poclcc)
 function __init__()
-    JLLWrappers.@generate_init_header(OpenCL_jll, OpenCL_Headers_jll, Hwloc_jll, Zstd_jll)
+    JLLWrappers.@generate_init_header(OpenCL_jll, OpenCL_Headers_jll, Hwloc_jll, Zstd_jll, SLEEF_jll)
     JLLWrappers.@init_library_product(
         libpocl,
         "lib/libpocl.so",
